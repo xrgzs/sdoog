2ff703d43efc40c7320bcc504bf0f1c0ceab9327
xrgzs <xiaoran@xrgzs.top>
2024-11-02 07:17:26 +0800

    feat: support lanzou list share with password

--- bin/utils.ps1
+++ bin/utils.ps1
@@ -3,7 +3,10 @@ function Get-LanzouList {
     param (
         [Parameter(Mandatory = $true, Position = 0)]
         [string]
-        $Uri
+        $Uri,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]
+        $Pass
     )
     $sharekey = $Uri -split '/' | Select-Object -Last 1
     $webcontent = Invoke-RestMethod -Uri "https://www.lanzoui.com/$sharekey" -UseBasicParsing
@@ -17,6 +20,7 @@ function Get-LanzouList {
     $body += "t=$([regex]::Match($webcontent, "var $($([regex]::Match($webcontent, "'t':(\w+)").Groups[1].Value)) = '(\w+)'").Groups[1].Value)"
     $body += "k=$([regex]::Match($webcontent, "var $($([regex]::Match($webcontent, "'k':(\w+)").Groups[1].Value)) = '(\w+)'").Groups[1].Value)"
     $body += "up=1"
+    if ($Pass) { $body += "pwd=$Pass" }
     $body = $body -join "&"
     $list = Invoke-RestMethod -Uri "https://www.lanzoui.com$ajaxm" -Method Post -Body $body
     return $list.text
