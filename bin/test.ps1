94ef9d6d05838311b7f73824a9c280bcb730b220
xrgzs <xiaoran@xrgzs.top>
2024-11-06 11:02:37 +0800

    (chore): fix test ci

--- bin/test.ps1
+++ bin/test.ps1
@@ -2,6 +2,12 @@
 #Requires -Modules @{ ModuleName = 'BuildHelpers'; ModuleVersion = '2.0.1' }
 #Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.2.0' }
 
+# Override JSON Schema File
+if (-not (Test-Path "$env:SCOOP_HOME\schema.json.bak")) {
+    Move-Item "$env:SCOOP_HOME\schema.json" "$env:SCOOP_HOME\schema.json.bak" -Force
+}
+Copy-Item (Join-Path $PSScriptRoot 'bin' 'schema.json') "$env:SCOOP_HOME\schema.json" -Force
+
 $pesterConfig = New-PesterConfiguration -Hashtable @{
     Run    = @{
         Path     = "$PSScriptRoot/.."
@@ -12,4 +18,7 @@ $pesterConfig = New-PesterConfiguration -Hashtable @{
     }
 }
 $result = Invoke-Pester -Configuration $pesterConfig
+
+Move-Item "$env:SCOOP_HOME\schema.json.bak" "$env:SCOOP_HOME\schema.json" -Force
+
 exit $result.FailedCount
