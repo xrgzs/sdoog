2695d523803c0872f209d5cc224338ae12f5b5a0
xrgzs <xiaoran@xrgzs.top>
2024-11-06 10:34:44 +0800

    (chore): Override JSON Schema File for ci

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,6 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+# Override JSON Schema File
+Move-Item "$env:SCOOP_HOME\schema.json" "$env:SCOOP_HOME\schema.json.bak" -Force
+Copy-Item (Join-Path $PSScriptRoot 'bin' 'schema.json') "$env:SCOOP_HOME\schema.json" -Force
 . "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
+Move-Item "$env:SCOOP_HOME\schema.json.bak" "$env:SCOOP_HOME\schema.json" -Force
